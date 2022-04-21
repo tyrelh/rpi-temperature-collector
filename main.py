1db95effce1f9010a67236961bf1934cdbb8d713
tyrelh <tyrel.hiebert@gmail.com>
2022-04-21 13:38:29 -0700

    add arg checking

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import sys
 import time
 import datetime
 import boto3
+from random import randint
 # from w1thermsensor import W1ThermSensor
 
 dynamodb = boto3.client('dynamodb') # TODO: add region name
 # sensor1 = W1ThermSensor
+
+def getSensorReading():
+    # TODO: build out real sensor reading
+    return randint(16, 30)
 
 
 def getDate(now):
@@ -28,10 +34,10 @@ def checkIfCurrentTableExists(tableName):
         Limit = 2
     )
     if "TableNames" in response:
-        if len(response["TableNames"]) == 1 tableName in response["TableNames"]:
+        if len(response["TableNames"]) == 1 and tableName in response["TableNames"]:
             if tableName in response["TableNames"]:
                 return True
-            elif
+            else:
                 return False
     raise Exception("There was an issue fetching dynamo tables", response)
 
@@ -48,30 +54,39 @@ def createNewTable(tableName):
                         "AttributeType": "S"
                     },
                     {
-                        "AttributeName": 
+                        "AttributeName": ""
                     }
                 ]
             )
 
 
 
-def main():
+def main(location):
     # get current time
     now = datetime.now()
     dateTime = getDateTime(now)
     # get current temperature from sensor
-    temperature = sensor.get_temperature()
+    temperature = getSensorReading()
     print("The temperature at %s for sensor1 is %s c" % dateTime.time, temperature)
 
     # check if table for today exists
     if not checkIfCurrentTableExists(dateTime.date):
         # create new table for today
         createNewTable(dateTime.date)
+        # lower provisioned throughput of older tables
 
-    # push stat to table
+    # push stat to today's table
 
 
-    
+
+
+
+def usage():
+    print("Usage: python3 temperatureSensor.py [location]")    
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit("Incorrect args.")
+    else:
+        main(sys.argv[1])
